6ff299e72e35da4d4150797e91e6457d24bf69aa
Jeffrey Mcintyre <redeyecoding@gmail.com>
2022-10-09 17:37:13 -0400

    Update redeye_bot.py
    changed to start using slackclient (Legacy Python Slack SDK)

--- redeye_bot.py
+++ redeye_bot.py
@@ -1,71 +1,24 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from slack_bolt import App
+from slack import WebClient
+from slack.errors import SlackApiError
 
 env_path = Path('.') / '.env'
 load_dotenv()
 
-# Initializes your app with your bot token and signing secret
-app = App(token=os.getenv("SLACK_BOT_TOKEN"), signing_secret=os.getenv("SLACK_SIGNING_SECRET"))
+slack_token = os.environ["SLACK_APP_TOKEN"]
+client = WebClient(token=slack_token)
 
+# Verify it works
+client = WebClient()
+api_response = client.api_test()
 
-# Add functionality here
-# @app.event("app_home_opened") etc
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-  try:
-    # views.publish is the method that your app uses to push a view to the Home tab
-    client.views_publish(
-      # the user that opened your app's app home
-      user_id=event["user"],
-      # the view object that appears in the app home
-      view={
-        "type": "home",
-        "callback_id": "home_view",
-
-        # body of the view
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    )
-
-
-
-  
-  except Exception as e:
-    logger.error(f"Error publishing home tab: {e}")
-
-# Start your app
-if __name__ == "__main__":
-    app.start(port=int(os.getenv("PORT")))
-
+try:
+  response = client.chat_postMessage(
+    channel="C0XXXXXX",
+    text="Hello from your app! :tada:"
+  )
+except SlackApiError as e:
+  # You will get a SlackApiError if "ok" is False
+  assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
