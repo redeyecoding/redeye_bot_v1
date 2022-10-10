1ce6dc8bd0c315edc82f439ac97b399a47849da8
Jeffrey Mcintyre <redeyecoding@gmail.com>
2022-10-10 18:38:20 -0400

    Update redeye_bot.py
    some changes.

--- redeye_bot.py
+++ redeye_bot.py
@@ -1,24 +1,21 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from slack import WebClient
-from slack.errors import SlackApiError
+import slack
+from flask import Flask
 
 env_path = Path('.') / '.env'
 load_dotenv()
 
-slack_token = os.environ["SLACK_APP_TOKEN"]
-client = WebClient(token=slack_token)
+app = Flask(__name__)
 
-# Verify it works
-client = WebClient()
-api_response = client.api_test()
+@app.route("/")
+def hello_world():
+    return "hello world redyeye"
 
-try:
-  response = client.chat_postMessage(
-    channel="C0XXXXXX",
-    text="Hello from your app! :tada:"
-  )
-except SlackApiError as e:
-  # You will get a SlackApiError if "ok" is False
-  assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
