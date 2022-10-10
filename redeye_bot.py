884596f9bac06e3bc87637c0e951022917f94987
Jeffrey Mcintyre <redeyecoding@gmail.com>
2022-10-10 18:57:58 -0400

    Update redeye_bot.py
    more changes

--- redeye_bot.py
+++ redeye_bot.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import slack
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
 from flask import Flask
 
 env_path = Path('.') / '.env'
@@ -9,9 +11,34 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello world redyeye"
+slack_web_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+slack_events_adapter = SlackEventAdapter(os.environ.get("SLACK_APP_TOKEN", "/slack/events", app))
+
+MESSAGE_BLOCK = {
+    "type": "section",
+    "text": {
+        "type": "mrkdwn",
+        "text": "",
+    },
+}
+
+
+@slack_events_adapter.on("message")
+def message(payload):
+    event = payload.get("event", {})
+
+    text = event.get("text")
+
+    if "hello redeye" in text.lower():
+        channel_id = event.get("channel")
+        bot_response_message = "Hello there from redeyebot"
+
+        MESSAGE_BLOCK["text"]["text"] = bot_response_message
+
+        block_package = {
+            "channel": channel_id,
+            "blocks": [MESSAGE_BLOCK]
+        }
 
 
 
